9ec47f16ae2793cc9b4b8903754851e41221300c
Taouil Gharbaoui <abdel.taouil.bk@hotmail.com>
2020-10-06 14:54:16 +0200

    fin du modal sans media query

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,2 @@
 import './modules/modecouleur.js'
+import './modules/modale.js'
--- public/js/modules/modecouleur.js
+++ public/js/modules/modecouleur.js
@@ -1,12 +1,16 @@
 let myBody = document.querySelector('body')
+let myModale = document.querySelectorAll('div')[2]
+
 
 let modeJour = document.querySelector('.modeblanc')
 let modeNuit = document.querySelector('.modesombre')
 
 modeJour.addEventListener('click',()=>{
     myBody.style = 'background-color: white'
+    myModale.style = 'background-color: white'
 })
 
 modeNuit.addEventListener('click',()=>{
     myBody.style = 'background-color: black; color: white'
+    myModale.style = 'background-color: black; color: white'
 })
