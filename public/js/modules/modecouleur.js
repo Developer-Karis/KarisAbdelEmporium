ea4881bb644a7fd72f6aabf9c6cc1f048925cabd
SEVERECLICK-dev <papadopouloskaris@gmail.com>
2020-10-08 12:01:21 +0200

    final commit Karis et Abdel

--- public/js/modules/modecouleur.js
+++ public/js/modules/modecouleur.js
@@ -1,19 +1,37 @@
-let myBody = document.querySelector('body')
+export let myBody = document.querySelector('body')
 let myModale = document.querySelectorAll('div')[2]
 let myModale2 = document.querySelectorAll('div')[10];
-
 
 let modeJour = document.querySelector('.modeblanc')
 let modeNuit = document.querySelector('.modesombre')
 
-modeJour.addEventListener('click',()=>{
+let myNavRow = document.querySelectorAll(".souligner");
+export let arrNavLi = Array.from(myNavRow);
+
+let myInfoCust = document.getElementsByClassName("infoCust");
+let arrInfoCust = Array.from(myInfoCust);
+
+export let verifColor = true;
+
+modeJour.addEventListener('click', () => {
+    verifColor = true;
     myBody.style = 'background-color: white'
     myModale.style = 'background-color: white'
     myModale2.style = 'background-color: white'
+    arrNavLi.forEach(element => {
+        element.style.color = "black";
+    });
 })
 
-modeNuit.addEventListener('click',()=>{
+modeNuit.addEventListener('click', () => {
+    verifColor = false;
     myBody.style = 'background-color: black; color: white'
     myModale.style = 'background-color: black; color: white;'
     myModale2.style = 'background-color: black; color: white;'
+    arrNavLi.forEach(element => {
+        element.style.color = "white";
+    });
+    arrInfoCust.forEach(element => {
+        element.style.color = "black";
+    });
 })
--- public/js/modules/navMedia.js
+++ public/js/modules/navMedia.js
@@ -1,15 +1,37 @@
 export let navBar = document.querySelector('nav');
 export let head = document.getElementsByTagName('header');
 export let stick = navBar.offsetTop;
+import { arrNavLi } from "./modecouleur.js";
+import { verifColor } from "./modecouleur.js";
 
-function fixnav(){
-    if(window.scrollY >= stick){
+function fixnav() {
+    if (window.scrollY >= stick) {
         document.body.style.paddingTop = navBar.offsetHeight + 'px';
         document.body.classList.add('stickyNav');
+        arrNavLi.forEach(element => {
+            element.style.color = "black";
+        });
     } else {
         document.body.style.paddingTop = 0;
         document.body.classList.remove('stickyNav');
     }
 }
 
-window.addEventListener('scroll', fixnav);
+function fixnav2() {
+    if (window.scrollY >= stick) {
+        document.body.style.paddingTop = navBar.offsetHeight + 'px';
+        document.body.classList.add('stickyNav');
+        arrNavLi.forEach(element => {
+            element.style.color = "white";
+        });
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('stickyNav');
+    }
+}
+
+if (verifColor == true) {
+    window.addEventListener('scroll', fixnav);
+} else if (verifColor === false) {
+    window.addEventListener('scroll', fixnav2);
+}
