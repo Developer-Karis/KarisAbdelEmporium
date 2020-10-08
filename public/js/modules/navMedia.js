ca679e63407a0ae68b9616d78a7a122ffd4f0358
Taouil Gharbaoui <abdel.taouil.bk@hotmail.com>
2020-10-08 10:31:42 +0200

    second fin du projet pour second fusion

--- public/js/modules/navMedia.js
+++ public/js/modules/navMedia.js
@@ -1,1 +1,15 @@
+export let navBar = document.querySelector('nav');
+export let head = document.getElementsByTagName('header');
+export let stick = navBar.offsetTop;
 
+function fixnav(){
+    if(window.scrollY >= stick){
+        document.body.style.paddingTop = navBar.offsetHeight + 'px';
+        document.body.classList.add('stickyNav');
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('stickyNav');
+    }
+}
+
+window.addEventListener('scroll', fixnav);
